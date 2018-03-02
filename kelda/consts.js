8850c9ed1e1392e7492fae3efd2397a6fb8c4f98
Kevin Lin <kevin@kelda.io>
2018-03-02 07:40:27 -0800

    kelda: Remove default floating IP

--- kelda/consts.js
+++ kelda/consts.js
@@ -1,13 +1,24 @@
-exports.floatingIP = '13.56.215.88';
+// The IP at which public-facing services will be available.
+exports.floatingIP = '<FLOATING IP>';
+if (exports.floatingIP === '<FLOATING IP>') {
+  throw new Error('a floating IP is required');
+}
+
+// The number of worker VMs to boot.
+exports.numWorkers = 3;
+
+// The number of SLURM nodes to boot.
+exports.slurmScale = 2;
+
+// The number of Keep store containers to boot.
+exports.keepScale = 2;
+
 exports.ipToCert = {
   [exports.floatingIP]: {
     cert: readFile('config/ssl/certificate.pem'),
     key: readFile('config/ssl/key.pem'),
   },
 };
-exports.numWorkers = 3;
-exports.slurmScale = 2;
-exports.keepScale = 2;
 
 function readFile(f) {
   return fs.readFileSync(path.join(__dirname, f), { encoding: 'utf8' });
